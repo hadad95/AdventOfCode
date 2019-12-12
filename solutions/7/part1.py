5da0bd1fe03e3442943c4fcfd1834af70699f5a9
Kef <a7medosama2222@gmail.com>
2019-12-12 13:50:55 +0200

    Part 1 of day 7

---
+++ solutions/7/part1.py
@@ -0,0 +1,121 @@
+class Int():
+    def __init__(self, inputs):
+        self.inputs = inputs.copy()
+        self.original_inputs = inputs.copy()
+        self.cursor = 0
+    
+    def run(self, ip):
+        self.cursor = 0
+        current_ip = 0
+        self.inputs = self.original_inputs.copy()
+        
+        while True:
+            settings = self.inputs[self.cursor].rjust(5, '0')
+            opcode = int(settings[-2:])
+            if opcode == 99:
+                print('HALT')
+                return
+
+            if opcode == 1:
+                param1 = int(self.inputs[self.cursor + 1])
+                param2 = int(self.inputs[self.cursor + 2])
+                param3 = int(self.inputs[self.cursor + 3])
+                val1 = int(self.inputs[param1]) if int(settings[-3]) == 0 else param1
+                val2 = int(self.inputs[param2]) if int(settings[-4]) == 0 else param2
+                self.inputs[param3] = str(val1 + val2)
+                self.cursor += 4
+            elif opcode == 2:
+                param1 = int(self.inputs[self.cursor + 1])
+                param2 = int(self.inputs[self.cursor + 2])
+                param3 = int(self.inputs[self.cursor + 3])
+                val1 = int(self.inputs[param1]) if int(settings[-3]) == 0 else param1
+                val2 = int(self.inputs[param2]) if int(settings[-4]) == 0 else param2
+                self.inputs[param3] = str(val1 * val2)
+                self.cursor += 4
+            elif opcode == 3:
+                param1 = int(self.inputs[self.cursor + 1])
+                self.inputs[param1] = ip[current_ip]
+                current_ip += 1
+                self.cursor += 2
+            elif opcode == 4:
+                param1 = int(self.inputs[self.cursor + 1])
+                val1 = int(self.inputs[param1]) if int(settings[-3]) == 0 else param1
+                return str(val1)
+                self.cursor += 2
+            elif opcode == 5:
+                param1 = int(self.inputs[self.cursor + 1])
+                param2 = int(self.inputs[self.cursor + 2])
+                val1 = int(self.inputs[param1]) if int(settings[-3]) == 0 else param1
+                val2 = int(self.inputs[param2]) if int(settings[-4]) == 0 else param2
+                if val1 != 0:
+                    self.cursor = val2
+                else:
+                    self.cursor += 3
+            elif opcode == 6:
+                param1 = int(self.inputs[self.cursor + 1])
+                param2 = int(self.inputs[self.cursor + 2])
+                val1 = int(self.inputs[param1]) if int(settings[-3]) == 0 else param1
+                val2 = int(self.inputs[param2]) if int(settings[-4]) == 0 else param2
+                if val1 == 0:
+                    self.cursor = val2
+                else:
+                    self.cursor += 3
+            elif opcode == 7:
+                param1 = int(self.inputs[self.cursor + 1])
+                param2 = int(self.inputs[self.cursor + 2])
+                param3 = int(self.inputs[self.cursor + 3])
+                val1 = int(self.inputs[param1]) if int(settings[-3]) == 0 else param1
+                val2 = int(self.inputs[param2]) if int(settings[-4]) == 0 else param2
+                val3 = int(self.inputs[param3]) if int(settings[-5]) == 0 else param3
+                if val1 < val2:
+                    self.inputs[param3] = '1'
+                else:
+                    self.inputs[param3] = '0'
+                
+                self.cursor += 4
+            elif opcode == 8:
+                param1 = int(self.inputs[self.cursor + 1])
+                param2 = int(self.inputs[self.cursor + 2])
+                param3 = int(self.inputs[self.cursor + 3])
+                val1 = int(self.inputs[param1]) if int(settings[-3]) == 0 else param1
+                val2 = int(self.inputs[param2]) if int(settings[-4]) == 0 else param2
+                val3 = int(self.inputs[param3]) if int(settings[-5]) == 0 else param3
+                if val1 == val2:
+                    self.inputs[param3] = '1'
+                else:
+                    self.inputs[param3] = '0'
+                
+                self.cursor += 4
+                
+            else:
+                print(f'INVALID OPCODE: {opcode}')
+        
+
+with open('./input', 'r') as f:
+    inputs = f.read().strip().split(',')
+
+prog = Int(inputs)
+total = 0
+
+current_config = [0, 0, 0, 0, 0]
+optimum_config = [0, 0, 0, 0, 0]
+
+def calc(index, loop, ip):
+    for i in loop:
+        global current_config
+        current_config[index] = i
+        out = prog.run([str(i), ip])
+        if index == 4:
+            global total
+            global optimum_config
+            if int(out) > total:
+                optimum_config = current_config.copy()
+                total = int(out)
+        else:
+            l = loop.copy()
+            l.remove(i)
+            calc(index + 1, l, out)
+
+calc(0, list(range(5)), '0')
+print(total)
+print(optimum_config)
