b81893e14f4f9121a020d3d207bf25ad3311355d
kef95 <theonlykef@gmail.com>
2020-09-04 01:22:13 +0200

    Properly utilizing recursion in day 1 pt 2

--- solutions/2019/01/part2.py
+++ solutions/2019/01/part2.py
@@ -1,19 +1,16 @@
 total = 0
 
 def calc_fuel(fuel):
-    global total
     result = int(fuel) // 3 - 2
     if result <= 0:
-        return
+        return 0
     
-    total += result
-    calc_fuel(result)
+    return result + calc_fuel(result)
     
-
 with open('./input', 'r') as f:
     inputs = f.read().strip().split('\n')
     
 for fuel in inputs:
-    calc_fuel(fuel)
+    total += calc_fuel(fuel)
 
 print(total)
